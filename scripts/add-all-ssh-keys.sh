d38784726f2fe04e9e4c8b0d22a7b1a587187da4
Patrick Huber <stackmagic@gmail.com>
2013-02-19 11:38:31 +0100

    that thing was broken

--- scripts/add-all-ssh-keys.sh
+++ scripts/add-all-ssh-keys.sh
@@ -27,8 +27,6 @@
 export SSH_ASKPASS=/usr/bin/ksshaskpass
 
 for identity in $(find ${HOME}/.ssh/ -type f -name "id_*" | grep -v ".pub"); do
-	if [ "$(ssh-add -l | cut -d" " -f3 | grep $identity)" == "" ]; then
-		ssh-add $identity
-	fi
+	[ -z "$(ssh-add -l | awk '{ print $3 }' | egrep "${identity}$")" ] && ssh-add "${identity}"
 done
 
